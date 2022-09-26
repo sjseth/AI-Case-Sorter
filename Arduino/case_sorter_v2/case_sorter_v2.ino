0e6cc0db3210e7e9cc863e01f8ce16a5faef2d34
Seth Hahner <sethhahner@hotmail.com>
2022-09-25 21:16:49 -0700

    Update case_sorter_v2.ino

--- Arduino/case_sorter_v2/case_sorter_v2.ino
+++ Arduino/case_sorter_v2/case_sorter_v2.ino
@@ -5,7 +5,7 @@
 //TB6600 PINS (STEPPIN IS PUL+ ON TB6000)
 //Stepper controller is set to 32 Microsteps
 #define FEED_DIRPIN 8
-#define FEED_STEPPIN 9
+#define FEED_STEPPIN 9  
 #define FEED_TB6600Enable 2
 #define FEED_MICROSTEPS 32
 
@@ -27,10 +27,20 @@ int sorterMotorCurrentPosition = 0;
 int sorterChutes[] ={0,17, 33, 49, 66, 83, 99, 116, 132}; 
 
 
+//inputs
+int feedSpeed = 60; //range: 1..100
+int feedSteps = 100; //range: 1..1000
+int sortSpeed = 60; //range: 1..100
+int sortSteps = 20; //range: 1..500 //20 default
+int feedPauseTime = 50; //range: 1.2000
+
+
 void setup() {
   Serial.begin(9600);
   pinMode(FEED_TB6600Enable, OUTPUT);
   pinMode(SORT_TB6600Enable, OUTPUT);
+   pinMode(FEED_DIRPIN, OUTPUT);
+  pinMode(FEED_STEPPIN, OUTPUT);
   digitalWrite(FEED_TB6600Enable, LOW);
   digitalWrite(SORT_TB6600Enable, LOW);
 
@@ -45,13 +55,45 @@ void loop() {
       
       //normal input would just be the tray # you want to sort to which would be something like "1\n" or "9\n" (\n)if you are using serial console, the \n is already included in the writeline
       //so just the number 1-10 would suffice. 
+      //the software on the other end should be listening for  "done\n". 
+      
+      //set feed speed. Values 1-100. Def 60
+      if(input.startsWith("feedspeed:")){
+         input.replace("feedspeed:","");
+         feedSpeed= input.toInt();
+          Serial.print("ok\n");
+         return;
+      }
+      
+      //set sort speed. Values 1-100. Def 60
+      if(input.startsWith("sortspeed:")){
+         input.replace("sortspeed:","");
+         sortSpeed= input.toInt();
+          Serial.print("ok\n");
+         return;
+      }
 
-     //the software on the other end should be listening for  "done\n". 
-     
-      if(input.startsWith("fs")){
-         input.replace("fs","");
-         indexSteps= input.toInt();
-         feedCycleSteps = indexSteps * FEED_MICROSTEPS * -1;
+      //set sort steps. Values 1-100. Def 20
+      if(input.startsWith("sortsteps:")){
+         input.replace("sortsteps:","");
+         sortSteps= input.toInt();
+          Serial.print("ok\n");
+         return;
+      }
+
+      //set feed steps. Values 1-1000. Def 100
+      if(input.startsWith("feedsteps:")){
+         input.replace("feedsteps:","");
+         feedSteps= input.toInt();
+          Serial.print("ok\n");
+         return;
+      }
+
+      //set feed steps. Values 1-1000. Def 100
+      if(input.startsWith("feedpausetime:")){
+         input.replace("feedpausetime:","");
+         feedPauseTime= input.toInt();
+          Serial.print("ok\n");
          return;
       }
 
@@ -65,7 +107,7 @@ void loop() {
          return;
       }
       
-      //too run feeder, send  any string starting with f:
+      //to run feeder, send  any string starting with f:
        if(input.startsWith("f")){
          input.replace("f","");
          runFeedMotorManual();
@@ -73,6 +115,13 @@ void loop() {
          return;
       }
       
+      //to run feeder, send  any string starting with f:
+       if(input.startsWith("f")){
+         input.replace("f","");
+         runFeedMotorManual();
+         Serial.print("done\n");
+         return;
+      }
 
       
       int sortPosition = input.toInt();
@@ -99,27 +148,35 @@ void runSorterMotor(int chute){
 
 
 void runFeedMotorManual(){
-  int steps=100;
+  
+  int steps=0;
   digitalWrite(FEED_DIRPIN, HIGH);
-  steps = abs(steps) * FEED_MICROSTEPS;
+  steps = abs(feedSteps) * FEED_MICROSTEPS;
+  Serial.print(steps);
+  int delayTime = 120 - feedSpeed;
+  Serial.print(delayTime);
   for(int i=0;i<steps;i++){
       digitalWrite(FEED_STEPPIN, HIGH);
-      delayMicroseconds(60);   //pulse
+      delayMicroseconds(60  );   //pulse
       digitalWrite(FEED_STEPPIN, LOW);
-      delayMicroseconds(20); //speed 156 = 1 second per revolution
+      delayMicroseconds(delayTime); //speed 156 = 1 second per revolution
   }
-  delay(50);
+  delay(feedPauseTime);
+  Serial.print("pause");
+  
   for(int i=0;i<steps;i++){
       digitalWrite(FEED_STEPPIN, HIGH);
       delayMicroseconds(60);   //pulse
       digitalWrite(FEED_STEPPIN, LOW);
-      delayMicroseconds(20); //speed 156 = 1 second per revolution
+      delayMicroseconds(delayTime); //speed 156 = 1 second per revolution
   }
   //Serial.print(steps);Serial.print("\n");
 }
 
 
 void runSortMotorManual(int steps){
+  int delayTime = 120 - sortSpeed;
+     
   if(steps>0){
     digitalWrite(SORT_DIRPIN, HIGH);
   }else{
@@ -128,9 +185,9 @@ void runSortMotorManual(int steps){
   steps = abs(steps) ;
   for(int i=0;i<steps;i++){
       digitalWrite(SORT_STEPPIN, HIGH);
-      delayMicroseconds(60);   //pulse
+      delayMicroseconds(60);   //pulse //def 60
       digitalWrite(SORT_STEPPIN, LOW);
-      delayMicroseconds(20); //speed 156 = 1 second per revolution
+      delayMicroseconds(delayTime); //speed 156 = 1 second per revolution //def 20
   }
   //Serial.print(steps);Serial.print("\n");
 }
